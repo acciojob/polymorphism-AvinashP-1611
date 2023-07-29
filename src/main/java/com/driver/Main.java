c354d3c1529df26d10e5e617dd7f7acb880d0998
Avinash Prajapati <avinashprajapati1611r@gmail.com>
2023-07-29 23:46:05 +0530

    push code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+  public static class Product{
+       public static void main(String[] args) {
+           Product p=new Product();
+           p.product(4,5);
+           p.product(4,5,6);
+           p.product(4.0d,5.0d);
+       }
+       public int product(int x, int y) {
+            return 1;
+       }
+       public int product(int x, int y, int z) {
+            return 2;
+       }
+       public double product(double x, double y) {
+          return 1.1;
+       }
+   }
 }
