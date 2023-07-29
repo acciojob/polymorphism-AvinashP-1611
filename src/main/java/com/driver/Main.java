3422da4b9c417d1eb434a1c452cd06de8a8b800a
AvinashP-1611 <134164909+AvinashP-1611@users.noreply.github.com>
2023-07-30 00:26:20 +0530

    Updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,21 @@
 package com.driver;
 
 public class Main {
-  public static class Product{
        public static void main(String[] args) {
            Product p=new Product();
-           p.product(4,5);
-           p.product(4,5,6);
-           p.product(4.0d,5.0d);
+          System.out.println(p.product(4,5));
+           System.out.println(p.product(4,5,6));
+           System.out.println(p.product(4.0d,5.0d));
        }
+      public static class Product{
        public int product(int x, int y) {
-            return 1;
+            return x*y;
        }
        public int product(int x, int y, int z) {
-            return 2;
+            return x*y*z;
        }
        public double product(double x, double y) {
-          return 1.1;
+          return x*y;
        }
    }
 }
